ce2fbf95bdd1653adf9a6a82828ab174952b025c
Abder-hbt <9907@holbertonstudents.com>
2025-01-16 14:24:59 +0000

    SUII

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -4,5 +4,4 @@ word_first_3 = word[0:3]
 word_last_2 = word[7:9]
 middle_word = word[1:8]
 print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+print(f"Last 2 letters: {word_last_2}\nMiddle word: {middle_word}")
