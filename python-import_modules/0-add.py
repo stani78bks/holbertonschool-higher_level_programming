ea97c1cd5f2ef1fd3d85fa539088bb34c97f36b0
Abder-hbt <9907@holbertonstudents.com>
2025-01-17 22:57:34 +0000

    SUIII

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 from add_0 import add
 
-if __name__ == "__main__":
 
+def main():
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    main()
