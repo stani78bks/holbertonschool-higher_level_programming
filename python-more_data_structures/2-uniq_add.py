35b1e5ba2ed0eeb8f98c8d6a218d4e31f5612017
Abder-hbt <9907@holbertonstudents.com>
2025-01-24 17:12:14 +0000

    SUII

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
+# 2-uniq_add.py
+
 def uniq_add(my_list=[]):
-    count_dict = {}  # Dictionnaire pour compter les occurrences de chaque élément
-    total = 0  # Initialiser la somme des éléments uniques
-
-    # Remplir le dictionnaire avec les occurrences de chaque élément
-    for i in my_list:
-        count_dict[i] = count_dict.get(i, 0) + 1
-
-    # Additionner uniquement les éléments qui apparaissent une seule fois
-    for i, count in count_dict.items():
-        if count == 1:  # Si l'élément apparaît une seule fois
-            total += i  # Ajouter l'élément unique à la somme
-
-    return total  # Retourner la somme des éléments uniques
+    result = 0
+    for x in set(my_list):
+        result += x
+    return (result)
