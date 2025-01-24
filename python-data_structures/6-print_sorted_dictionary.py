aec60a39ce47ec13dc7f9b0f49c31d81f33044c3
Abder-hbt <9907@holbertonstudents.com>
2025-01-24 16:33:58 +0000

    SUII

--- python-data_structures/6-print_sorted_dictionary.py
+++ python-data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print('{}'.format('\n'.join([' '.join(['{:d}'.format(i)
-          for i in r]) for r in matrix])))
+    if matrix:
+        for elems in matrix:
+            i = 1
+            length = len(elems)
+
+            for elem in elems:
+                if i == length:
+                    print('{:d}'.format(elem), end='')
+                else:
+                    print('{:d}'.format(elem), end=' ')
+                i += 1
+
+            print()
