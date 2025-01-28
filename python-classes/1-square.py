01090518b7006ea2d130261a47fbeab99c0093ac
Abder-hbt <9907@holbertonstudents.com>
2025-01-28 14:04:29 +0000

    (°O°)SUIIIII

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-square.py
+
+"""Def une Instance size."""
+
+
+class Square:
+    """Représente une Instance size."""
+
+    def __init__(self, size):
+        self.__size = size
