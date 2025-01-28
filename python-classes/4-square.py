07c3661870c1ce1fba1d96aebc0ba20a7904e3dc
Abder-hbt <9907@holbertonstudents.com>
2025-01-28 15:38:48 +0000

    SUIII

---
+++ python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# 4-square.py
+
+"""Def une Instance area."""
+
+
+class Square:
+    """Représente une Instance area = size**2."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Renvoie l'aire du carré."""
+        return self.__size ** 2
