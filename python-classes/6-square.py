2d40593b35fce5a36c52bb26d9ba196dc83ecabd
Abder-hbt <9907@holbertonstudents.com>
2025-01-29 14:34:04 +0000

    SUII

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 5-square.py
+# 6-square.py
 
 """Def une Instance area."""
 
@@ -7,12 +7,13 @@
 class Square:
     """Représente une Instance area = size**2."""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        self.position = position
 
     @property
     def size(self):
@@ -30,10 +31,26 @@ class Square:
         """Renvoie l'aire du carré."""
         return self.__size ** 2
 
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(i, int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def my_print(self):
         """Renvoie un carré à partir de l'aire"""
-        if self.__size == 0:
+        if self.size == 0:
             print("")
-        else:
-            for i in range(self.__size):
-                print(self.__size * "#")
+            return
+
+        for i in range(self.position[1]):
+            print("")
+
+        for r in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
