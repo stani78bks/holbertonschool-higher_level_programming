b137bf01ca42cffed49d27c2861e1091fa6949aa
Abder-hbt <9907@holbertonstudents.com>
2025-01-31 16:26:41 +0000

    SUIIII

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -52,4 +52,4 @@ class Rectangle:
             return rectangle.rstrip()
 
     def __repr__(self):
-        return f'Rectangle("{self.__width}", "{self.__height}")'
+        return f"Rectangle({self.__width}, {self.__height})"
