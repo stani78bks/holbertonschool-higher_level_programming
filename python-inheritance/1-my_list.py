1fd6013cf4f3bc2d513ccba640eb4bb3bdffc002
Abder-hbt <9907@holbertonstudents.com>
2025-02-04 15:21:39 +0000

    SUIII

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 
-
 class MyList(list):
     """Classe qui hérite de list et ajoute une méthode pour trier la liste"""
     def print_sorted(self):
         """Affiche la liste triée sans la modifier"""
         print(sorted(self))
-
