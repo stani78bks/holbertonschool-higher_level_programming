517ed1936296505e01bd633bad9126b28efa07a0
Abder-hbt <9907@holbertonstudents.com>
2025-02-04 14:55:34 +0000

    SUII

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""
-Module MyList
-Ce module définit une classe MyList qui hérite de list et ajoute une méthode print_sorted().
-"""
+# 1-my_list.py
+"""Fonction qui return une MYlist"""
 
-class MyList(list):
-    """
-    Classe MyList qui hérite de la classe list.
-    Elle ajoute une méthode print_sorted() pour afficher une version triée de la liste.
-    """
 
+class Mylist(list):
+    """Def d'une classe"""
     def print_sorted(self):
-        """
-        Affiche une version triée de la liste.
-        La liste d'origine n'est pas modifiée.
-        """
+
+        """print obj"""
         print(sorted(self))
