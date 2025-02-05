1493e08e97062112cf4e35cf2becd1d7299e94ca
Abder-hbt <9907@holbertonstudents.com>
2025-02-05 14:49:50 +0000

    SUII

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 2-is_same_class.py
+"""ref une fonction"""
+
+
+def inherits_from(obj, a_class):
+
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
