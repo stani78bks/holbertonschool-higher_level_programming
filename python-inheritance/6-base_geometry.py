799fefc28ad9c1edb6687b21d551983cb1be79e6
Abder-hbt <9907@holbertonstudents.com>
2025-02-05 15:19:09 +0000

    SUII

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 2-is_same_class.py
+"""ref une fonction"""
+
+
+class BaseGeometry:
+    """Base"""
+    def area(self):
+        """Area"""
+        raise Exception("area() is not implemented")
