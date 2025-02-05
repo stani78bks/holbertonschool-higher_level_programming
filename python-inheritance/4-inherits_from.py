40863ce1f6ba085a5e100a3897b6216a1dae87b5
Abder-hbt <9907@holbertonstudents.com>
2025-02-05 15:03:20 +0000

    SUII

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,6 +5,15 @@
 
 def inherits_from(obj, a_class):
 
+    """
+    Checks if an object `
+
+    Args:.
+
+    Returns:
+              (dir
+    """
+
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
