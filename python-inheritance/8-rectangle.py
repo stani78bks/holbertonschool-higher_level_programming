6c864a38883ae001893298027ad005828cb07c7b
Abder-hbt <9907@holbertonstudents.com>
2025-02-06 14:55:57 +0000

    SUII3

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# 8-rectangle.py
+"""Définition de la classe BaseGeometry avec des méthodes de validation."""
+
+
+class BaseGeometry:
+    """Classe de base pour la géométrie."""
+
+    def area(self):
+        """Lève une exception car cette méthode doit
+        être implémentée dans les sous-classes."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Valide que value est un entier strictement positif.
+
+        Args:
+            name (str): Nom du paramètre (pour le message d'erreur).
+            value (int): Valeur à vérifier.
+
+        Raises:
+            TypeError: Si value n'est pas un entier.
+            ValueError: Si value est inférieur ou égal à 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
