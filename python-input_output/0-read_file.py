5f93e935655b608fa00936c13e34141547b27800
Abder-hbt <9907@holbertonstudents.com>
2025-02-12 16:02:30 +0000

    SUUUI

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
+
 # Définition de la fonction qui prend un nom de fichier en paramètre
-def read_file(filename=""):  
-    # Ouverture du fichier en mode lecture avec 'with', qui garantit sa fermeture automatique
-    with open(filename) as file:  
+def read_file(filename=""):
+    # Ouverture du fichier en mode lecture avec
+    # 'with', qui garantit sa fermeture automatique
+    with open(filename) as file:
         # Lecture du contenu du fichier
-        content = file.read()  
+        content = file.read()
         # Affichage du contenu du fichier dans la console
-        print(content)  
+        print(content)
