c103322f62b2ccd824d46678e0b514eacad497db
Abder-hbt <9907@holbertonstudents.com>
2025-02-14 12:17:48 +0100

    Add files via upload

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains the read_file function
+"""
+def read_file(filename=""):
+    """""reads a text file(UTF8) and prints it to stdout
+
+    args:
+        filename: Name of the file read
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
