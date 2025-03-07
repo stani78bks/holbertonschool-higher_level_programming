843257fc9d1d385d255da938442896005941deac
Abder-hbt <9907@holbertonstudents.com>
2025-03-07 15:09:52 +0000

    SUII

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,33 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    # Vérification de la validité des arguments
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <username> <password> <database>")
+        sys.exit(1)
+    
+    try:
+        # Connexion à la base de données
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+        
+        with db.cursor() as cur:
+            # Exécution de la requête
+            cur.execute("SELECT * FROM states ORDER BY id")
+            
+            # Récupération des résultats et affichage
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+    
+    except MySQLdb.MySQLError as e:
+        print(f"Error: {e}")
+    finally:
+        # Fermeture de la connexion à la base de données
+        if db:
+            db.close()
