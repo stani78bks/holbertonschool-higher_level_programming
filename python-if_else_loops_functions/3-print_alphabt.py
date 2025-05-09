cd104c8719b5a9aee1cf0248b6a5a696bfabddfc
stani78bks <10648@holbertonstudents.com>
2025-05-09 02:02:31 +0200

    python

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("{}".format("".join([chr(c) for c in range(97, 123) if c != 101 and c != 113])), end="")
+print("{}".format("".join(
+    [chr(c) for c in range(97, 123) if c != 101 and c != 113])), end="")
