c5cb86dd7732c88a138b0dabb657f82277a32366
root <root@HBTN-IDF-P7>
2025-05-15 11:29:38 +0200

    ex

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            division = my_list_1[i] / my_list_2[i]
+        except (ZeroDivisionError, IndexError, TypeError, ValueError):
+            division = 0
+        result.append(division)
+    return result
