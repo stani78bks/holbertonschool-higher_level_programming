999ce8279f6e13ede46b73c9d9f13c07aeb509e1
stani78bks <10648@holbertonstudents.com>
2025-05-21 15:05:50 +0200

    Fix say_my_name implementation and update test file

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,7 +3,15 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints 'My name is <first name> <last name>'."""
+    """Prints 'My name is <first name> <last name>'.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str): The last name (optional).
+
+    Raises:
+        TypeError: If either first_name or last_name is not a string.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
