60d2364f9bbd6a3b4e07af5f2137767a3cc79118
root <root@HBTN-IDF-P7>
2025-05-27 14:56:20 +0200

    geo

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+Defines a class BaseGeometry with area method.
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class with area method."""
+
+    def area(self):
+        """Raises an Exception indicating that the area method is not implemented."""
+        raise Exception("area() is not implemented")
