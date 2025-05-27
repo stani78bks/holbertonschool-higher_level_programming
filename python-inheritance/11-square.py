8cabce2429bc16b4ad3b2ba031010fe8b6fa80ab
root <root@HBTN-IDF-P7>
2025-05-27 16:45:49 +0200

    tt

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -5,22 +5,18 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square, inherits from Rectangle."""
+    """Represent a square, inherits from Rectangle."""
 
     def __init__(self, size):
-        """Initialize a new Square.
+        """Initialize a Square.
 
         Args:
-            size (int): The size of the square sides.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is <= 0.
+            size (int): The size of the square's sides.
         """
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
 
     def __str__(self):
-        """Return the square description in the format [Square] <width>/<height>."""
+        """Return the string representation of the Square."""
         return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
