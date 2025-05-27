add3d2f390ff4935d802b6b523bd46f575d9c604
root <root@HBTN-IDF-P7>
2025-05-27 14:15:02 +0200

    list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines a subclass MyList of list."""
-
-
+"""Defines the MyList class that inherits from list."""
 class MyList(list):
-    """Subclass of list with a method to print sorted list."""
+    """Custom list class that inherits from built-in list."""
 
     def print_sorted(self):
         """Prints the list in ascending sorted order."""
