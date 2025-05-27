ad3cfd45c54e87dd70ebba32a6a83f0dd15dd5d2
root <root@HBTN-IDF-P7>
2025-05-27 14:03:15 +0200

    list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module that defines a class MyList that inherits from list"""
+"""Defines a class MyList that inherits from list."""
 
 class MyList(list):
-    """A custom list class that inherits from list"""
+    """Custom list class that inherits from Python's built-in list."""
 
     def print_sorted(self):
-        """Prints the list in ascending sorted order"""
+        """Prints the list in ascending sorted order (without modifying original)."""
         print(sorted(self))
