1364de4e432d2fdc55525872ffeecfba29c3bdf9
stani78bks <10648@holbertonstudents.com>
2025-06-04 03:19:34 +0200

    rr

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Module 0-read_file
+This module contains a function to read and print the content of a UTF-8 text file.
+"""
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+    Args:
+        filename (str): The name of the file to read.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
