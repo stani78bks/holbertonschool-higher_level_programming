28e33b2c51e78fcdf3fcc4b23e10d287c287e039
root <root@HBTN-IDF-P7>
2025-06-06 10:57:17 +0200

    Add class_to_json function

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,6 +2,7 @@
 """
 Function that returns the dictionary description for JSON serialization
 """
+
 def class_to_json(obj):
     """Returns the dictionary description of an object"""
     return obj.__dict__
