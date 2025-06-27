a369287c3bf1df106465ae4495b6e397cac344ba
root <root@HBTN-IDF-P7>
2025-06-27 16:47:36 +0200

    select

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>
+0-select_states.py
+
+Lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
+    """
+    Main function:
+    that executes the database
+    connection and retrieval of states.
+    """
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
     )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
+    for state in states:
+        print(state)
 
-    for row in rows:
-        print(row)
-
-    cur.close()
+    cursor.close()
     db.close()
