0d0dc98c5be4440a73ec1fc076f1c08858f36814
root <root@HBTN-IDF-P7>
2025-06-27 15:54:47 +0200

    select

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-'''
-This module  lists all states from the database hbtn_0e_0_usa
-'''
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
+    # Récupération des arguments de la ligne de commande
+    user = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3]
+    db_name = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
+    # Connexion à la base de données
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=db_name)
+
+    # Création du curseur
     cursor = db.cursor()
+
+    # Exécution de la requête SQL
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cursor.fetchall()
 
-    for state in states:
-        print(state)
+    # Récupération des résultats et affichage
+    for row in cursor.fetchall():
+        print(row)
 
+    # Fermeture du curseur et de la connexion
     cursor.close()
     db.close()
